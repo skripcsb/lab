2608d3ab79557c4496e0e67161cf32d964b4652f
skripcsb <153450540+skripcsb@users.noreply.github.com>
2024-09-21 22:34:33 +0300

    Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+public class Palindrome {
+
+    public static String reverseString(String s) {
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed += s.charAt(i);
+        }
+        return reversed;
+    }
+
+    public static boolean isPalindrome(String s) {
+        String reversed = reverseString(s);
+        return s.equals(reversed);
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            if (isPalindrome(s)) {
+                System.out.println(s + " - это палиндром.");
+            } else {
+                System.out.println(s + " - это не палиндром.");
+            }
+        }
+    }
+}
